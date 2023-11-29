fa514e21003f18eed48ef853c4fc578cd3c747da
Marvin-Kabugi <kabugijeff@gmail.com>
2023-11-30 01:10:42 +0300

    added djoser

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'LittleLemonAPI.apps.LittlelemonapiConfig',
 ]
 
@@ -123,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+}
